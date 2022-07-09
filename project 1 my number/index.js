42ac970e303b9fc62d60e687574090c9d0b87cf3
smitasharma9981 <106658761+smitasharma9981@users.noreply.github.com>
2022-07-09 14:28:47 +0530

    Add files via upload

---
+++ project 1 my number/index.js
@@ -0,0 +1,13 @@
+"use strict";
+/*console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "correct number !";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);*/
+
+/*document.querySelector(".check").addEventListener("click", function () {
+  console.log(document.querySelector("guess").value);
+});*/
